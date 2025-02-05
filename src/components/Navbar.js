981dd88002e800884dea56c9a4b940e3af519718
johnchmura <jchmura@hawk.iit.edu>
2025-02-04 21:59:45 -0600

    basic navbar setup

---
+++ src/components/Navbar.js
@@ -0,0 +1,23 @@
+import Link from "next/link";
+import styles from "@/styles/Navbar.module.css";
+
+export default function Navbar() {
+  return (
+    <nav className={styles.navbar}>
+      <div className={styles.logo}>
+        <Link href="/">MyApp</Link>
+      </div>
+      <ul className={styles.navLinks}>
+        <li>
+          <Link href="/">Home</Link>
+        </li>
+        <li>
+          <Link href="/about">About</Link>
+        </li>
+        <li>
+          <Link href="/contact">Contact</Link>
+        </li>
+      </ul>
+    </nav>
+  );
+}
