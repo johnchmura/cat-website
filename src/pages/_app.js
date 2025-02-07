6dc5ca2c5c3e4de71d7494eec4aa7645c8154dfb
johnchmura <jchmura@hawk.iit.edu>
2025-02-06 20:41:57 -0600

    Some more css stuff

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,11 +1,16 @@
 import "@/styles/globals.css";
 import "@fontsource/poppins";
+//import "@fontsource/inter";
 import Navbar from "@/components/Navbar";
 
 export default function App({ Component, pageProps }) {
   return (
     <>
       <Navbar />
+      <link
+          href="https://fonts.googleapis.com/css2?family=Inter:wght@600;700&display=swap"
+          rel="stylesheet"
+        />
       <Component {...pageProps} />
     </>
   );
--- src/pages/index.js
+++ src/pages/index.js
@@ -7,21 +7,19 @@ import pawPrints from '../images/Paw_Prints.png'
 export default function Home() {
   return (
     <>
-      <Head>
+       <Head>
         <title>Vitek Family Farm</title>
         <meta name="description" content="A family website for adopting kittens!" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
       </Head>
       <div className={styles.rectangle}>
         <div className={styles.quote}>
-          <h1>"From Our Farm 
-              to Your Heart, 
-              Meet Your Baby!"
-          </h1>
-          </div>
+          <h1>"From Our Farm to Your Heart, Meet Your Baby!"</h1>
+        </div>
       </div>
-      <Image src={pawPrints} alt="Paw Prints" className={styles.pawPrints} draggable="false" onMouseDown={(e) => e.preventDefault()}/>
-      <Image src={catModel} alt="Kitten Model" className={styles.catModel} draggable="false" onMouseDown={(e) => e.preventDefault()}/>
+      <Image src={pawPrints} alt="Paw Prints" className={styles.pawPrints} draggable="false" />
+      <Image src={catModel} alt="Kitten Model" className={styles.catModel} draggable="false" />
+      <div className={styles.bar}></div>
 
     </>
   );
